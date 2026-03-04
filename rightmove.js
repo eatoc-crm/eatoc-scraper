84d4d59e24f35ab665348494e173154f1bcbf066
eatoc-crm <davidbritton@brittonestates.co.uk>
2026-03-04 20:32:10 +0000

    Update rightmove.js

--- rightmove.js
+++ rightmove.js
@@ -212,3 +212,16 @@ function extractRightmoveId(url) {
 }
 
 module.exports = { scrapeRightmove };
+browser = await puppeteer.launch({
+  headless: 'new',
+  executablePath: process.env.PUPPETEER_EXECUTABLE_PATH || '/usr/bin/chromium',
+  args: [
+    '--no-sandbox',
+    '--disable-setuid-sandbox',
+    '--disable-dev-shm-usage',
+    '--disable-gpu',
+    '--no-first-run',
+    '--no-zygote',
+    '--single-process'
+  ]
+});
